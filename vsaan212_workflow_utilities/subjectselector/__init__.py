4e5a8351fd2c3d05e130b31ca47e1a77a68e3d13
vsaan212 <van212@gmail.com>
2025-11-07 02:19:17 -0500

    Add files via upload

---
+++ vsaan212_workflow_utilities/subjectselector/__init__.py
@@ -0,0 +1,9 @@
+from .subjectselector import ComfyUI_subjectselector
+
+NODE_CLASS_MAPPINGS = {
+    "Vsaan_SubjectSelector": ComfyUI_subjectselector
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Vsaan_SubjectSelector": "Subject Selector"
+}
